e17437bdf73150f2295b8135f611befa8624963a
Andrew Hyder <andrew.hyder@gsa.gov>
2020-09-30 17:07:53 -0700

    data parsing and card improvements

--- app/models/bulk_sorn_parser.rb
+++ app/models/bulk_sorn_parser.rb
@@ -1,57 +1,67 @@
 class BulkSornParser
   def self.work
-    begin
+    parsed_response = HTTParty.get('https://www.govinfo.gov/bulkdata/PAI/2019/PAI-2019-GSA.xml').parsed_response
+    agency = Agency.find_or_create_by(name: parsed_response["pai"]["agency"]["name"])
+    sorns = parsed_response["pai"]["agency"]["section"]
+    sorns.each do |sorn|
+      system_number = sorn["systemNumber"]
 
-      xml = File.open('data/PAI-2019-GSA.xml').read
-      json_str = Hash.from_xml(xml).to_json
-      json = JSON.parse(json_str)
+      # Almost all of the subsections are an array
+      if sorn["subsection"].class == Array
+        system_name = sorn["subsection"].shift["__content__"]
+          # transform the subsections
+        subsections = {}
+        sorn["subsection"].each do |subsection|
+          content = subsection["xhtmlContent"].try(:fetch, "p")
 
-      agency = Agency.find_or_create_by(name: json["pai"]["agency"]["name"])
-      sorns = json["pai"]["agency"]["section"]
-      sorns.each do |sorn|
-        system_number = sorn["systemNumber"]
-        if sorn["subsection"].class == Array
-          system_name = sorn["subsection"].shift
-            # transform the subsections
-          subsections = {}
-          sorn["subsection"].each {|subsection| subsections[subsection["type"]] = subsection["xhtmlContent"]["p"] }
 
-          Sorn.create!(
-            agency: agency,
-            system_number: system_number,
-            system_name: system_name,
-            security: subsections["securityClassification"],
-            location: subsections["systemLocation"],
-            manager: subsections["systemManager"],
-            authority: subsections["authorityForMaintenance"],
-            purpose: subsections["purpose"],
-            categories_of_individuals: subsections["categoriesOfIndividuals"],
-            categories_of_record: subsections["categoriesOfRecords"],
-            source: subsections["recordSourceCategories"],
-            routine_uses: subsections["routineUsesOfRecords"],
-            storage: subsections["storage"],
-            retrieval: subsections["retrievability"],
-            retention: subsections["retentionAndDisposal"],
-            safeguards: subsections["safeguards"],
-            access: subsections["recordAccessProcedures"],
-            contesting: subsections["contestingRecordProcedures"],
-            notification: subsections["notificationProcedure"],
-            exemptions: subsections["exemptionsClaimed"],
-            history: subsections["history"]
-          )
-        else
-          system_name = sorn["subsection"]
-          Sorn.create!(
-            agency: agency,
-            system_number: system_number,
-            system_name: system_name
-          )
+          if content.class == Hash
+            content = content.values
+          end
+
+          if content.class == Array
+            content = content.map do |p|
+              p if p.class == String
+            end.compact
+
+            content = content.join
+          end
+
+          subsections[subsection["type"]] = content
         end
+
+        Sorn.create!(
+          agency: agency,
+          system_number: system_number,
+          system_name: system_name,
+          security: subsections["securityClassification"],
+          location: subsections["systemLocation"],
+          manager: subsections["systemManager"],
+          authority: subsections["authorityForMaintenance"],
+          purpose: subsections["purpose"],
+          categories_of_individuals: subsections["categoriesOfIndividuals"],
+          categories_of_record: subsections["categoriesOfRecords"],
+          source: subsections["recordSourceCategories"],
+          routine_uses: subsections["routineUsesOfRecords"],
+          storage: subsections["storage"],
+          retrieval: subsections["retrievability"],
+          retention: subsections["retentionAndDisposal"],
+          safeguards: subsections["safeguards"],
+          access: subsections["recordAccessProcedures"],
+          contesting: subsections["contestingRecordProcedures"],
+          notification: subsections["notificationProcedure"],
+          exemptions: subsections["exemptionsClaimed"],
+          history: subsections["history"]
+        )
+      else
+        system_name = sorn["subsection"]["__content__"]
+        Sorn.create!(
+          agency: agency,
+          system_number: system_number,
+          system_name: system_name
+        )
       end
-    rescue => exception
-
-      binding.pry
     end
-
+    puts "All done"
   end
 end
