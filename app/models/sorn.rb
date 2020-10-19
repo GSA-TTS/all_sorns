90ed97a0b1409e6730b7522fcfd857aa28c7cdd4
Andrew Hyder <andrew.hyder@gsa.gov>
2020-10-19 11:16:11 -0700

    Quick checkbox select defaults for demo.

--- app/models/sorn.rb
+++ app/models/sorn.rb
@@ -58,7 +58,6 @@ class Sorn < ApplicationRecord
     self.update(**parsed_sorn)
   end
 
-
   # https://prsanjay.wordpress.com/2015/07/15/export-to-csv-in-rails-select-columns-names-dynamically/
   def self.to_csv(columns = column_names, options = {})
     CSV.generate(options) do |csv|
