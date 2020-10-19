3d5977a5c4d7c6d5b68d592381c31730f68c4966
Peter Rowland <peter.rowland@gsa.gov>
2020-10-19 16:15:34 -0700

    add total count

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,4 +1,9 @@
 class AnalyticsController < ApplicationController
     def index
+
+        @total_sorns = Sorn.count
+
+        @agency_sorn_counts = Sorn.group(:agency_names).count
+
     end
 end
