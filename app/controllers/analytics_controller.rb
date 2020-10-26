0b4490434b2c3b2145907ffe6e62243d08331c3b
Peter Rowland <peter.rowland@gsa.gov>
2020-10-26 13:45:05 -0700

    minor changes

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,7 +2,6 @@ class AnalyticsController < ApplicationController
     def index
 
         @total_sorns = Sorn.count
-
         @agency_sorn_counts = Sorn.group(:agency_names).count.sort_by{ |name, count| count }.reverse
 
     end
