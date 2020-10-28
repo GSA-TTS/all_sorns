c4754e43ae474bfceb168107f686389a83a84b21
Peter Rowland <peter.rowland@gsa.gov>
2020-10-28 15:03:44 -0700

    repair count by agency query

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,7 +2,18 @@ class AnalyticsController < ApplicationController
     def index
 
         @total_sorns = Sorn.count
-        @agency_sorn_counts = Sorn.group(:agency_names).count.sort_by{ |name, count| count }.reverse
+        
+        #Per agency stats
+        #count of SORNs per agency
+        @agency_sorn_counts = Sorn.except(:order).group(:agency_names).count.sort_by{ |name, count| count }.reverse
+
+        #count of different types of SORNs - new,modified, rescinded, matching agreements, others
+
+        #Data quality stats
+        #How many records don't have titles?
+        #How many records are missing ___ field?
+
+        #which SORNs don't have XML links?post
 
     end
 end
