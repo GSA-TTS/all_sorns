2c313a57a590509b46425cff11d5dafa0a5adb16
Andrew Hyder <andrew.hyder@gsa.gov>
2020-11-13 15:37:07 -0800

    Remove unused migration

--- db/migrate/20201109232203_add_mentioned_sorns_to_sorn.rb
+++
@@ -1,5 +0,0 @@
-class AddMentionedSornsToSorn < ActiveRecord::Migration[6.0]
-  def change
-    add_column :sorns, :mentioned, :string, array: true, default: []
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -94,7 +94,6 @@ ActiveRecord::Schema.define(version: 2020_11_13_200542) do
     t.string "text_url"
     t.string "publication_date"
     t.string "title"
-    t.string "mentioned", default: [], array: true
     t.index "to_tsvector('english'::regconfig, (access)::text)", name: "access_idx", using: :gist
     t.index "to_tsvector('english'::regconfig, (action)::text)", name: "action_idx", using: :gist
     t.index "to_tsvector('english'::regconfig, (addresses)::text)", name: "addresses_idx", using: :gist
