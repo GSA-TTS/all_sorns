70ac7e675f9f35dafbfb71836f388850a4484da0
Andrew Hyder <andrew.hyder@gsa.gov>
2020-12-09 12:47:22 -0800

    WIP

--- app/javascript/packs/agencies.js
+++ app/javascript/packs/agencies.js
@@ -25,17 +25,27 @@ $( function () {
   // Also ensures they are always included in the search request
   agencyList.on('searchComplete', function() {
     showChecked();
+    sortChecked();
   })
 
   function showChecked () {
+    // make a list
     agencyList.items.forEach(agency => {
       // agency.elm is the container <div class="usa-checkbox">
       // agency.elm.children[0] is the checkbox
       checkbox = agency.elm.children[0]
       if ( checkbox.checked ) {
         agency.show();
-        $(agency.elm).prependTo("#selected-agencies");
+        // $(agency.elm).prependTo("#selected-agencies");
       }
+    })
+  }
+
+  function sortChecked(){
+    checked = $("#agencies :checked")
+    console.log(checked)
+    checked.each(function(i, obj){
+      $(obj).parent().prependTo("#selected-agencies");
     })
   }
 })
