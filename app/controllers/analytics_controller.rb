ce31879952649583b03b9c0703036e9580fb6789
Peter Rowland <peter.rowland@gsa.gov>
2020-12-09 10:57:05 -0800

    put stats in hash - unpack in table

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -4,19 +4,11 @@ class AnalyticsController < ApplicationController
     @sorn_types = Sorn.pluck(:action_type).tally
     @agencies = Agency.all
 
-    #count of different types of SORNs - new,modified, rescinded, matching agreements, others
-
     #Data quality stats
-    #How many records don't have titles?
-    #How many records are missing ___ field?
-
-        #Data quality stats
-        #How many records don't have titles?
-        #How many records are missing ___ field?
-
-        #which SORNs don't have XML links?
-        #select count(*) from sorns where xml_url is null
-        @no_xml_url = Sorn.where(xml_url: nil).count
+    @data_stats = {}
+    @data_stats.merge!('No xml url': Sorn.where(xml_url: nil).count)
+    @data_stats.merge!('Sorns with list-able CORs': Sorn.where("categories_of_record ~* ?", '•', ).count)
+  
 
     end
 end
