2688f2840327c33bee7a94584614391ce48a532a
Laura Gerhardt <laura.gerhardt@gsa.gov>
2020-12-15 16:59:03 -0500

    remove migration report and violation uri

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -15,7 +15,7 @@ Rails.application.config.content_security_policy do |policy|
   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
   # Specify URI for violation reports
-  policy.report_uri "/csp-violation-report-endpoint"
+  # policy.report_uri "/csp-violation-report-endpoint"
 end
 
 # If you are using UJS then enable automatic nonce generation
@@ -27,4 +27,4 @@ end
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-Rails.application.config.content_security_policy_report_only = true
+# Rails.application.config.content_security_policy_report_only = true
