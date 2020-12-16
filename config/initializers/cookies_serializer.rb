57b74fcdc4a2df1283d9ddf463b705b260130876
Laura Gerhardt <laura.gerhardt@gsa.gov>
2020-12-16 18:08:03 -0500

    Zap fix (#72)
    * add noreferrer
    
    * add csrf
    
    * remove csrf
    
    * update manifest to productio for no cache control
    
    * enable csp with google allowed
    
    * update samesite
    
    * remove samesite proc
    
    * Update cookies_serializer.rb
    
    * update ssl install
    
    * update source doc
    
    * add no opener to source external link
    
    * allow for csp report
    
    * remove migration report and violation uri
    
    * move gtag into js to make csp happy

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -3,3 +3,5 @@
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
 Rails.application.config.action_dispatch.cookies_serializer = :json
+
+Rails.application.config.action_dispatch.cookies_same_site_protection = :strict
