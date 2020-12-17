d965ed6cee1343ee5668819f4120787de5a57c0f
Laura Gerhardt <laura.gerhardt@gsa.gov>
2020-12-17 09:30:25 -0500

    Update config/schedule.rb
    Co-authored-by: Andrew Hyder <andrew.hyder@gsa.gov>

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-every 1.day, at: '4:30 am' do
+every 1.day, at: '10:30 pm' do
     rake "cf:on_first_instance federal_register:find_sorns"
     rake "cf:on_first_instance all_sorns:update_all_mentioned_sorns"
   end
