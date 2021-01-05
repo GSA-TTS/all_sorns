2a513489da5858e4b52f230be35393c245036b30
Laura Gerhardt <laura.gerhardt@gsa.gov>
2021-01-05 17:02:38 -0500

    revert to old sorn

--- app/controllers/sorns_controller.rb
+++ app/controllers/sorns_controller.rb
@@ -1,23 +1,15 @@
 class SornsController < ApplicationController
   def search
-
-    if !no_params_on_page_load?
-      if dates_blank?
-        puts 'holy cow'
-      end
-      # initialize on search first
-      populate_parameters()
-    end
-  end
-
-  private
-
-  def populate_parameters
-    
     @sorns = Sorn.no_computer_matching.includes(:mentioned).preload(:agencies)
-    
-    if params[:fields].blank? && dates_blank?
-      initial_broad_search()
+
+    if no_params_on_page_load?
+      # return all sorns with default fields
+      @selected_fields = Sorn::DEFAULT_FIELDS
+
+    elsif params[:fields].blank?
+      # Return nothing, with no default fields
+      @selected_fields = nil
+      @sorns = Sorn.none
 
     elsif search_and_agency_blank?
       #  return all sorns with just those fields
@@ -48,6 +40,18 @@ class SornsController < ApplicationController
       raise "WUT"
     end
 
+    if params[:starting_year].present?
+      # from beginning of start year
+      starting_date = params[:starting_year] + "-01-01"
+      @sorns = @sorns.where('publication_date::DATE > ?', starting_date)
+    end
+
+    if params[:ending_year].present?
+      # to end of ending year
+      ending_date = params[:ending_year] + "-12-31"
+      @sorns = @sorns.where('publication_date::DATE < ?', ending_date)
+    end
+
     if multiword_search?
       @sorns = @sorns.only_exact_matches(params[:search], @selected_fields)
       @sorns = Kaminari.paginate_array(@sorns).page(params[:page]) if request.format == :html
@@ -62,33 +66,14 @@ class SornsController < ApplicationController
     end
   end
 
-  def initial_broad_search
-    # place where can optimize future initial searches
-    @selected_fields = Sorn::FIELDS
-    field_syms = @selected_fields.map { |field| field.to_sym }
-    @sorns = @sorns.dynamic_search(field_syms, params[:search])
-  end
-
-  def date_filter
-    if params[:starting_year].present?
-      # from beginning of start year
-      starting_date = params[:starting_year] + "-01-01"
-      @sorns = @sorns.where('publication_date::DATE > ?', starting_date)
-    end
-
-    if params[:ending_year].present?
-      # to end of ending year
-      ending_date = params[:ending_year] + "-12-31"
-      @sorns = @sorns.where('publication_date::DATE < ?', ending_date)
-    end
-  end
+  private
 
   def multiword_search?
     params[:search].scan(/\w+/).size > 1 if params[:search].present?
   end
 
   def no_params_on_page_load?
-    params[:search].blank? && params[:fields].blank? && params[:agencies].blank? && dates_blank?
+    params[:search].blank? && params[:fields].blank? && params[:agencies].blank?
   end
 
   def search_and_agency_blank?
@@ -105,9 +90,5 @@ class SornsController < ApplicationController
 
   def search_and_fields_and_agency_present?
     params[:search].present? && params[:fields].present? && params[:agencies].present?
-  end
-
-  def dates_blank?
-    (params[:starting_date].blank? || params[:ending_year].blank)
   end
 end
