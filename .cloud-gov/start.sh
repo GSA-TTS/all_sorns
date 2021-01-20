d2ff20bf2376b1774035f773d1fd8f6751f15003
Andrew Hyder <andrew.hyder@gsa.gov>
2021-01-20 11:25:00 -0800

    Clean up files

--- .cloud-gov/start.sh
+++ .cloud-gov/start.sh
@@ -1,5 +1,2 @@
 bundle exec rake cf:on_first_instance db:migrate
-if [ "$1" = "update" ] ; then
-    bundle exec rails cf:on_first_instance federal_register:find_sorns
-fi
 bundle exec rails s -p $PORT -e production
