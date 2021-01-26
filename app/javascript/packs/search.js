08128894f5008dccf455f36bca451cf4e6314c44
Peter Rowland <peter.rowland@gsa.gov>
2021-01-26 12:01:01 -0800

    fixed display

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -75,13 +75,16 @@ function publicationDateValidation(){
     $("#starting_year")[0].setCustomValidity("Sorry, this tool only contains SORNs starting from 1994. Please enter a later starting year");
   } else {
     $("#starting_year")[0].setCustomValidity('');
-    $badge = $("#active-date-range")
-    $badge.prepend(`${startYear} - ${endYear}`)
-    $filterSection = $('#active-date-filter')
-    if ($filterSection.is(":hidden") ){
-      $filterSection.show();
-
-      $badge.css("display", "inline-block");
+    
+    if(startYear && endYear) {
+      $filterSection = $('#active-date-filter')
+      $badge = $("#active-date-range")
+      $dates = $("#active-date-range span")
+      $dates.text(`${startYear} - ${endYear}`)
+      if ($filterSection.is(":hidden") ){
+        $filterSection.show();
+        $badge.css("display", "inline-block");
+      }
     }
   }
 
