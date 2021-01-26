bc61d31ba41f781df196c46c4bbb2f58f5b83724
Laura Gerhardt <laura.gerhardt@gsa.gov>
2021-01-26 16:41:22 -0500

    address remaining zaps

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
