3007f4973fe409853bca7ef1deb57d664d462a6d
Peter Rowland <peter.rowland@gsa.gov>
2021-01-28 15:09:11 -0800

    fill in start or end date if not populated

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -25,17 +25,18 @@ $( function () {
  })
  
  //Add badge for date filters if populated
- if(parseInt($("#starting_year").val()) && parseInt($("#ending_year").val())) {
-   startYear = parseInt($("#starting_year").val())
-   endYear = parseInt($("#ending_year").val())
-   $filterSection = $('#active-date-filter')
-   $badge = $("#active-date-range")
-   $dates = $("#active-date-range span")
-   $dates.text(`${startYear} - ${endYear}`)
-   if ($filterSection.is(":hidden") ){
-     $filterSection.show();
-     $badge.css("display", "inline-block");
-   }
+ if(parseInt($("#starting_year").val()) || parseInt($("#ending_year").val())) {
+      publicationDateValidation()
+  //  startYear = parseInt($("#starting_year").val())
+  //  endYear = parseInt($("#ending_year").val())
+  //  $filterSection = $('#active-date-filter')
+  //  $badge = $("#active-date-range")
+  //  $dates = $("#active-date-range span")
+  //  $dates.text(`${startYear} - ${endYear}`)
+  //  if ($filterSection.is(":hidden") ){
+  //    $filterSection.show();
+  //    $badge.css("display", "inline-block");
+  //  }
  }
 
  // Listener for checkboxes
@@ -98,13 +99,18 @@ function publicationDateValidation(){
  } else {
    $("#starting_year")[0].setCustomValidity('');
    // If dates are valid, create badge
+   if (isNaN(startYear)){
+     startYear = "1994"
+   } else if (isNaN(endYear)){
+    d = new Date();
+    endYear = d.getFullYear()
+   }
    createDatesFilter(startYear, endYear)
    }
  }
  
 
  function createDatesFilter(startYear, endYear){
-  console.log(startYear)
   $filterSection = $('#active-date-filter')
   $badge = $("#active-date-range")
   $dates = $("#active-date-range span")
