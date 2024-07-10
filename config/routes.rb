64d79e5d9233141d770c26e13a7cfcbdd1d5ac2e
Bret Mogilefsky <bmogilefsky@gmail.com>
2024-07-10 14:19:35 -0700

    Ran `rails:app_update` for 6.1

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about', format: false
 
   get '/help', to: 'pages#help', format: false
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
