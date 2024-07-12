8285c3d657acac91cceac64f1b07e027a05a9c4d
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2024-07-11 20:55:50 -0700

    Add @hotwired/turbo-rails to JS

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @7.1.0
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @7.3.0
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @6.1.7
