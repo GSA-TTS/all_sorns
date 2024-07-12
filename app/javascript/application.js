d7cd170e193b91411a1275dcb2e68e0fc1761a75
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2024-07-12 16:40:00 -0700

    Remove call for jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // import "@hotwired/turbo-rails"
 
-// require("@rails/ujs").start()
-require("jquery")
-
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
