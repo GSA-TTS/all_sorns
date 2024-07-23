f05170a87e4820cb1c62404d942fae00c6829ebf
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-07-23 17:42:39 -0400

    Remove duplicative setup

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,6 @@ module AllSorns
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.active_support.cache_format_version = 7.0
-    config.active_support.disable_to_s_conversion = true
 
     config.active_job.queue_adapter = :good_job
     # Configuration for the application, engines, and railties goes here.
