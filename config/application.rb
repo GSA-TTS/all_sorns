50aeae0dd8eab21a0e7a9d164d54e88a5430b7eb
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-07-23 17:33:43 -0400

    Update to 7.0 config defaults

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ Bundler.require(*Rails.groups)
 module AllSorns
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
+    config.active_support.cache_format_version = 7.0
+    config.active_support.disable_to_s_conversion = true
 
     config.active_job.queue_adapter = :good_job
     # Configuration for the application, engines, and railties goes here.
