f07a2f2692d320ffb3a6bae1686da64c22c8fc26
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-07-24 09:15:24 -0400

    Fix non-system tests
    * address deprecation warning with `fixture_path`
    * Raise all exceptions in request tests to keep a test passing that was relying on that behavior

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
-  # Render exception templates for rescuable exceptions and raise for other exceptions.
-  config.action_dispatch.show_exceptions = :rescuable
+  # Raise for all exceptions in tests
+  config.action_dispatch.show_exceptions = :none
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
