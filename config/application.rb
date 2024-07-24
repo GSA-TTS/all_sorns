7924e301de9668f1304a17c4b10f89eed0d6eef7
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-07-24 09:40:15 -0400

    Update to rails 7.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module AllSorns
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     config.active_job.queue_adapter = :good_job
 
